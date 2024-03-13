8204b27e30d4eb0b109d9e8acb2d29902d5d8e5b
Fanpeng Li <serforfp@gmail.com>
2024-03-13 21:14:17 +0000

    Created project

---
+++ src/main/java/com/fanpeng/fullstackCRUDspringboottest/FullstackCrudSpringbootTestApplication.java
@@ -0,0 +1,13 @@
+package com.fanpeng.fullstackCRUDspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackCrudSpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackCrudSpringbootTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fanpeng/fullstackCRUDspringboottest/FullstackCrudSpringbootTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fanpeng.fullstackCRUDspringboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackCrudSpringbootTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
